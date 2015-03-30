4edb53be1d9b52701d8a4d50f2263d25fec36f28
chanderdeep <ch.yadav430@gmail.com>
2015-03-30 11:25:25 +1000

    Initial commit

---
+++ src/Patients.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+/**
+ * Patients - records several patients
+ * Created by akki on 22/03/2015.
+ */
+public class Patients {
+    ArrayList<Patient> records;
+
+    public Patients() {
+
+        records = new ArrayList<Patient>();
+    }
+
+    public int count() {
+        return records.size();
+    }
+
+    public void add(Patient patient) {
+        records.add(patient);
+    }
+
+    public Patient get(int i) {
+        return records.get(i);
+    }
+
+    public boolean isEmpty() {
+        return records.isEmpty();
+    }
+}
+
+
